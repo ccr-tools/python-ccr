17ab5e850232012c9fe6bfd4bcf2fa6bb75accd1
george <rpubaddr0@gmail.com>
2012-05-08 17:32:33 -0600

    added search, msearch, info options, and an example in __main__

--- ccr.py
+++ ccr.py
@@ -2,11 +2,10 @@
 # A simple Python lib to access the Chakra Community Repository
 # Should work in python>=2.7, including python3
 
-__all__ = ["maintainer", "category", "votes"]
+__all__ = [ "search", "info", "msearch" ]
 
 import json
 import contextlib
-import urllib
 import urllib2
 
 
@@ -24,7 +23,9 @@ class Struct(dict):
 
 CCR_RPC = "http://chakra-linux.org/ccr/rpc.php?type="
 ARG = "&arg="
-
+SEARCH = "search"
+INFO = "info"
+MSEARCH = "msearch"
 
 
 def get_ccr_json(method, arg):
@@ -35,7 +36,37 @@ def get_ccr_json(method, arg):
         return json.loads(text.read(), object_hook=Struct)
 
 
+def search(keywords):
+    """search for some keywords - returns results as a list"""
+    results = get_ccr_json(SEARCH, keywords)
+    return results.results
+
+
+def info(package):
+    """return information for a specific package - returns results as a list"""
+    results = get_ccr_json(INFO, package)
+    return results.results
+
+
+def msearch(maintainer):
+    """search for packages owned by 'maintainer' - returns results as a list"""
+    results = get_ccr_json(MSEARCH, maintainer)
+    return results.results
+
+
+
 
 
 if __name__ == "__main__":
-    print(maintainer("snort"))
+    r = info("snort")
+    print("Name           : %s" % r.Name)
+    print("Version        : %s" % r.Version)
+    print("URL            : %s" % r.URL)
+    print("License        : %s" % r.License)
+    print("Category       : %s" % r.Category)
+    print("Maintainer     : %s" % r.Maintainer)
+    print("Description    : %s" % r.Description)
+    print("OutOfDate      : %s" % r.OutOfDate)
+    print("Votes          : %s" % r.NumVotes)
+
+
