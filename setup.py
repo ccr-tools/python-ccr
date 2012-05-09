79febaec0246b646b34f8b0c70bcd4d5f4f7f0c8
george <rpubaddr0@gmail.com>
2012-05-09 11:24:20 -0600

    added start of a setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from ccr import __version__
+
+long_description = open('README').read()
+
+setup(name='ccr',
+      version=__version__,
+      py_modules=['ccr'],
+      description='Library for accessing and working with the CCR.',
+      author='ccr-tools',
+      license='GPL3',
+      url='http://github.com/ccr-tools/python-ccr/',
+      long_description=long_description,
+      platforms=['any'],
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: GPL3 License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Internet",
+                   ]
+      )
