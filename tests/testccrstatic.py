4eced38d80f99c06b964e14ea7526c78ee84eac4
Guillaume <michaudg@gmail.com>
2014-02-12 21:03:16 -0500

    Some changes with known values

--- tests/testccrstatic.py
+++ tests/testccrstatic.py
@@ -10,19 +10,36 @@ import ccr
 
 class TestCCRStatic(unittest.TestCase):
 
-    pkg_list = ["cdrtools", "ls++-git"]
-    maintainer = "Guillaume"
+    MAINTAINER = "Guillaume"
+    KNOWN_VALUES = {
+        "cdrtools": {
+            "test_geturl": "packages.php?ID=2745",
+            "test_getpkgurl": "packages/cd/cdrtools/cdrtools.tar.gz",
+            "test_getpkgbuild": "pkgbuild_view.php?p=cdrtools",
+            "test_getpkgbuildraw": "packages/cd/cdrtools/cdrtools/PKGBUILD",
+            "test_getfileraw": "packages/cd/cdrtools/cdrtools/test.raw"
+        },
+        "ls++-git": {
+            "test_geturl": "packages.php?ID=3870",
+            "test_getpkgurl": "packages/ls/ls++-git/ls++-git.tar.gz",
+            "test_getpkgbuild": "pkgbuild_view.php?p=ls++-git",
+            "test_getpkgbuildraw": "packages/ls/ls++-git/ls++-git/PKGBUILD",
+            "test_getfileraw": "packages/ls/ls++-git/ls++-git/test.raw"
+        }
+    }
 
     def test_search(self):
-        results = ccr.search(self.pkg_list[1])
-        self.assertGreaterEqual(len(results), 1)
+        for packagename in self.KNOWN_VALUES:
+            results = ccr.search(packagename)
+            self.assertGreaterEqual(len(results), 1)
 
     def test_info(self):
-        results = ccr.info(self.pkg_list[1])
-        self.assertEqual(results.Name, self.pkg_list[1])
+        for packagename in self.KNOWN_VALUES:
+            results = ccr.info(packagename)
+            self.assertEqual(results.Name, packagename)
 
     def test_msearch(self):
-        results = ccr.msearch(self.maintainer)
+        results = ccr.msearch(self.MAINTAINER)
         self.assertGreaterEqual(len(results), 1)
 
     def test_list_orphans(self):
@@ -34,36 +51,30 @@ class TestCCRStatic(unittest.TestCase):
         results = ccr.getlatest()
         self.assertEqual(len(results.results), 10)
 
-    # values in known_values must match pkg_list array.
     def test_geturl(self):
-        known_values = ["packages.php?ID=2745", "packages.php?ID=3870"]
-        for packagename, value in zip(self.pkg_list, known_values):
+        for packagename in self.KNOWN_VALUES:
             result = ccr.geturl(packagename)
-            self.assertEqual(result, ccr.CCR_BASE + value)
+            self.assertEqual(result, ccr.CCR_BASE + self.KNOWN_VALUES[packagename][inspect.stack()[0][3]])
 
     def test_getpkgurl(self):
-        known_values = ["packages/cd/cdrtools/cdrtools.tar.gz", "packages/ls/ls++-git/ls++-git.tar.gz"]
-        for packagename, value in zip(self.pkg_list, known_values):
+        for packagename in self.KNOWN_VALUES:
             result = ccr.getpkgurl(packagename)
-            self.assertEqual(result, ccr.CCR_BASE + value)
+            self.assertEqual(result, ccr.CCR_BASE + self.KNOWN_VALUES[packagename][inspect.stack()[0][3]])
 
     def test_getpkgbuild(self):
-        known_values = ["pkgbuild_view.php?p=cdrtools", "pkgbuild_view.php?p=ls++-git"]
-        for packagename, value in zip(self.pkg_list, known_values):
+        for packagename in self.KNOWN_VALUES:
             result = ccr.getpkgbuild(packagename)
-            self.assertEqual(result, ccr.CCR_BASE + value)
+            self.assertEqual(result, ccr.CCR_BASE + self.KNOWN_VALUES[packagename][inspect.stack()[0][3]])
 
     def test_getpkgbuildraw(self):
-        known_values = ["packages/cd/cdrtools/cdrtools/PKGBUILD", "packages/ls/ls++-git/ls++-git/PKGBUILD"]
-        for packagename, value in zip(self.pkg_list, known_values):
+        for packagename in self.KNOWN_VALUES:
             result = ccr.getpkgbuildraw(packagename)
-            self.assertEqual(result, ccr.CCR_BASE + value)
+            self.assertEqual(result, ccr.CCR_BASE + self.KNOWN_VALUES[packagename][inspect.stack()[0][3]])
 
     def test_getfileraw(self):
-        known_values = ["packages/cd/cdrtools/cdrtools/test.raw", "packages/ls/ls++-git/ls++-git/test.raw"]
-        for packagename, value in zip(self.pkg_list, known_values):
+        for packagename in self.KNOWN_VALUES:
             result = ccr.getfileraw(packagename, "test.raw")
-            self.assertEqual(result, ccr.CCR_BASE + value)
+            self.assertEqual(result, ccr.CCR_BASE + self.KNOWN_VALUES[packagename][inspect.stack()[0][3]])
 
 
 if __name__ == '__main__':
