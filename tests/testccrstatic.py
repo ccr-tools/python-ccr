3f039a5341fa2559ab1d162553f02a14354533e8
Guillaume <michaudg@gmail.com>
2014-03-07 19:17:30 -0500

    Added dictionary to hold request.get.return_value.text data

--- tests/testccrstatic.py
+++ tests/testccrstatic.py
@@ -7,77 +7,87 @@ from ccr.ccr import *
 
 class TestCCRStatic(unittest.TestCase):
 
-    MAINTAINER = "Guillaume"
-    KNOWN_VALUES = {
-        "cdrtools": {
-            "test_geturl": "packages.php?ID=2745",
-            "test_getpkgurl": "packages/cd/cdrtools/cdrtools.tar.gz",
-            "test_getpkgbuild": "pkgbuild_view.php?p=cdrtools",
-            "test_getpkgbuildraw": "packages/cd/cdrtools/cdrtools/PKGBUILD",
-            "test_getfileraw": "packages/cd/cdrtools/cdrtools/test.raw",
-        },
-        "ls++-git": {
-            "test_geturl": "packages.php?ID=3870",
-            "test_getpkgurl": "packages/ls/ls++-git/ls++-git.tar.gz",
-            "test_getpkgbuild": "pkgbuild_view.php?p=ls++-git",
-            "test_getpkgbuildraw": "packages/ls/ls++-git/ls++-git/PKGBUILD",
-            "test_getfileraw": "packages/ls/ls++-git/ls++-git/test.raw",
-        },
-    }
-    requests.get = Mock()
+    @classmethod
+    def setUpClass(cls):
+        cls.maintainer = "Guillaume"
+        cls.known_values = {
+            "cdrtools": {
+                "test_geturl": "packages.php?ID=2745",
+                "test_getpkgurl": "packages/cd/cdrtools/cdrtools.tar.gz",
+                "test_getpkgbuild": "pkgbuild_view.php?p=cdrtools",
+                "test_getpkgbuildraw": "packages/cd/cdrtools/cdrtools/PKGBUILD",
+                "test_getfileraw": "packages/cd/cdrtools/cdrtools/test.raw",
+            },
+            "ls++-git": {
+                "test_geturl": "packages.php?ID=3870",
+                "test_getpkgurl": "packages/ls/ls++-git/ls++-git.tar.gz",
+                "test_getpkgbuild": "pkgbuild_view.php?p=ls++-git",
+                "test_getpkgbuildraw": "packages/ls/ls++-git/ls++-git/PKGBUILD",
+                "test_getfileraw": "packages/ls/ls++-git/ls++-git/test.raw",
+            },
+        }
+        requests.get = Mock()
+        cls.req_get_return_values = {
+            "test_search": '{"type":"mocked_type","results":{"ID":"mocked_ID"}}',
+            "test_info": '{"type":"mocked_type","results":{"ID":"mocked_ID","Name":"%s"}}',
+            "test_msearch": '{"type":"mocked_type","results":{"ID":"mocked_ID"}}',
+            "test_list_orphans": '{"type":"mocked_type","results":{"ID":"mocked_ID"}}',
+            "test_getlatest": '{"type":"mocked_type","results":[{"ID":"mocked_ID"},{"ID":"mocked_ID"},'
+                              '{"ID":"mocked_ID"},{"ID":"mocked_ID"},{"ID":"mocked_ID"},{"ID":"mocked_ID"},'
+                              '{"ID":"mocked_ID"},{"ID":"mocked_ID"},{"ID":"mocked_ID"},{"ID":"mocked_ID"}]}',
+            "test_geturl": '{"type":"mocked_type","results":{"ID":"%s"}}',
+        }
 
     def test_search(self):
-        for packagename in self.KNOWN_VALUES:
-            requests.get.return_value.text = '{"type":"mocked_type","results":{"ID":"mocked_ID"}}'
+        for packagename in self.known_values:
+            requests.get.return_value.text = self.req_get_return_values[inspect.stack()[0][3]]
             results = search(packagename)
         self.assertGreaterEqual(len(results), 1)
 
     def test_info(self):
-        for packagename in self.KNOWN_VALUES:
-            requests.get.return_value.text = '{"type":"mocked_type","results":{"ID":"mocked_ID","Name":"%s"}}' % packagename
+        for packagename in self.known_values:
+            requests.get.return_value.text = self.req_get_return_values[inspect.stack()[0][3]].replace("%s", packagename)
             results = info(packagename)
             self.assertEqual(results.Name, packagename)
 
     def test_msearch(self):
-        requests.get.return_value.text = '{"type":"mocked_type","results":{"ID":"mocked_ID"}}'
-        results = msearch(self.MAINTAINER)
+        requests.get.return_value.text = self.req_get_return_values[inspect.stack()[0][3]]
+        results = msearch(self.maintainer)
         self.assertGreaterEqual(len(results), 1)
 
     def test_list_orphans(self):
-        requests.get.return_value.text = '{"type":"mocked_type","results":{"ID":"mocked_ID"}}'
+        requests.get.return_value.text = self.req_get_return_values[inspect.stack()[0][3]]
         results = list_orphans()
         self.assertGreaterEqual(len(results), 1)
 
     def test_getlatest(self):
-        requests.get.return_value.text = '{"type":"mocked_type","results":[{"ID":"mocked_ID"},{"ID":"mocked_ID"},' \
-                                         '{"ID":"mocked_ID"},{"ID":"mocked_ID"},{"ID":"mocked_ID"},{"ID":"mocked_ID"},' \
-                                         '{"ID":"mocked_ID"},{"ID":"mocked_ID"},{"ID":"mocked_ID"},{"ID":"mocked_ID"}]}'
+        requests.get.return_value.text = self.req_get_return_values[inspect.stack()[0][3]]
         results = getlatest()
         self.assertEqual(len(results.results), 10)
 
     def test_geturl(self):
-        for packagename in self.KNOWN_VALUES:
-            requests.get.return_value.text = '{"type":"mocked_type","results":{"ID":"%s"}}'\
-                                             % self.KNOWN_VALUES[packagename][inspect.stack()[0][3]][-4:]
+        for packagename in self.known_values:
+            requests.get.return_value.text = self.req_get_return_values[inspect.stack()[0][3]].replace\
+                    ("%s", self.known_values[packagename][inspect.stack()[0][3]][-4:])
             result = geturl(packagename)
-            self.assertEqual(result, CCR_BASE + self.KNOWN_VALUES[packagename][inspect.stack()[0][3]])
+            self.assertEqual(result, CCR_BASE + self.known_values[packagename][inspect.stack()[0][3]])
 
     def test_getpkgurl(self):
-        for packagename in self.KNOWN_VALUES:
+        for packagename in self.known_values:
             result = getpkgurl(packagename)
-            self.assertEqual(result, CCR_BASE + self.KNOWN_VALUES[packagename][inspect.stack()[0][3]])
+            self.assertEqual(result, CCR_BASE + self.known_values[packagename][inspect.stack()[0][3]])
 
     def test_getpkgbuild(self):
-        for packagename in self.KNOWN_VALUES:
+        for packagename in self.known_values:
             result = getpkgbuild(packagename)
-            self.assertEqual(result, CCR_BASE + self.KNOWN_VALUES[packagename][inspect.stack()[0][3]])
+            self.assertEqual(result, CCR_BASE + self.known_values[packagename][inspect.stack()[0][3]])
 
     def test_getpkgbuildraw(self):
-        for packagename in self.KNOWN_VALUES:
+        for packagename in self.known_values:
             result = getpkgbuildraw(packagename)
-            self.assertEqual(result, CCR_BASE + self.KNOWN_VALUES[packagename][inspect.stack()[0][3]])
+            self.assertEqual(result, CCR_BASE + self.known_values[packagename][inspect.stack()[0][3]])
 
     def test_getfileraw(self):
-        for packagename in self.KNOWN_VALUES:
+        for packagename in self.known_values:
             result = getfileraw(packagename, "test.raw")
-            self.assertEqual(result, CCR_BASE + self.KNOWN_VALUES[packagename][inspect.stack()[0][3]])
+            self.assertEqual(result, CCR_BASE + self.known_values[packagename][inspect.stack()[0][3]])
