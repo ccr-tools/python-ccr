c362e30582771f2b06c81ffd64bc35d8ffc04392
george <rpubaddr0@gmail.com>
2014-05-20 16:02:12 -0600

    Try building an older version.

--- .build_kdebindings.sh
+++ .build_kdebindings.sh
@@ -1,9 +1,10 @@
 # Build and install the Python3 bindings for KDE4.
-wget http://download.kde.org/stable/4.13.1/src/pykde4-4.13.1.tar.xz
-tar xf pykde4-4.13.1.tar.xz
+version=4.8.5
+wget http://download.kde.org/stable/${version}/src/pykde4-${version}.tar.xz
+tar xf pykde4-${version}.tar.xz
 mkdir build && cd build
 export PYTHONDONTWRITEBYTECODE="TRUE"
-cmake ../pykde4-4.13.1 \
+cmake ../pykde4-${version} \
     -DCMAKE_BUILD_TYPE=Release \
     -DKDE4_BUILD_TESTS=OFF \
     -DCMAKE_INSTALL_PREFIX=/usr \
@@ -14,4 +15,3 @@ cmake ../pykde4-4.13.1 \
     -DWITH_Soprano=OFF
 make
 sudo make install
-sudo ln -s /usr/bin/pykdeuic4-3.4 /usr/bin/pykdeuic4
