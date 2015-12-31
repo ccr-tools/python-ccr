3bbcadd2c0c605641d1baa407c1ed13cc4967d68
rshipp <github@rshipp.com>
2015-12-31 21:28:15 +0100

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,32 @@
 from setuptools import find_packages, setup
+import os
 from ccr import __version__
 
+requirements = open(os.path.join(os.path.dirname(__file__),
+            'requirements.txt')).read()
+requires = requirements.strip().split('\n')
 
-setup(name = 'ccr',
-      packages = find_packages(),
-      version = __version__,
-      author = 'ccr-tools',
-      # FIXME create a valid ccr-tools support email
-      author_email = 'ccr-tools@chakra-project.org',
-      url = 'http://github.com/ccr-tools/python-ccr/',
-      description = 'Library for accessing and working with the CCR.',
-      long_description = open('README.rst').read(),
-      classifiers = ["Programming Language :: Python",
-                     "Programming Language :: Python :: 3",
-                     "License :: OSI Approved :: GPLv2 or any later version",
-                     "Operating System :: OS Independent",
-                     "Development Status :: 4 - Beta",
-                     "Intended Audience :: Developers",
-                     "Topic :: System :: Software Distribution",
-                   ],
-      license = 'GPLv2 or any later version',
-      platforms = ['any'],
-      test_suite = 'tests'
+setup(
+    name = 'ccr',
+    packages = find_packages(),
+    install_requires=requires,
+    version = __version__,
+    author = 'ccr-tools',
+    # FIXME create a valid ccr-tools support email
+    author_email = 'rshipp@chakraos.org',
+    url = 'http://github.com/ccr-tools/python-ccr/',
+    description = 'Library for accessing and working with the CCR.',
+    long_description = open('README.rst').read(),
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv2 or any later version",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Software Distribution",
+    ],
+    license = 'GPLv2 or any later version',
+    platforms = ['any'],
+    test_suite = 'tests'
 )
