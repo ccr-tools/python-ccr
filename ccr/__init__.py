ce323de799f878a71393744a7e22cec413ffc3fa
rshipp <github@rshipp.com>
2016-01-03 18:52:08 +0100

    Bump version to 0.3.1

--- ccr/__init__.py
+++ ccr/__init__.py
@@ -1,7 +1,7 @@
 from ccr.ccr import *
 from ccr.session import *
 
-__version__ = 0.3
+__version__ = "0.3.1"
 __all__ = ['search', 'info', 'msearch', 'list_orphans',
            "latest", "url", "pkg_url", "pkgbuild_url",
            "pkgbuild_raw_url", "file_raw_url",
